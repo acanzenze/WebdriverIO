2f89247e63e353001a33999d205290662f1c8825
alexandre.canzenze <alexandrecanzenze@hotmail.com>
2025-02-19 00:15:24 +0100

    first commit

---
+++ test/specs/my-first-test.spec.ts
@@ -0,0 +1,30 @@
+import { multiremote } from 'webdriverio'
+
+it("",async () => {
+    const browser = await multiremote({
+        myChromeBrowser: {
+            capabilities: {
+                browserName: 'chrome'
+            }
+        },
+        myFirefoxBrowser: {
+            capabilities: {
+                browserName: 'firefox'
+            }
+        }
+    })
+
+    // open url with both browser at the same time
+    await browser.url('http://json.org')
+
+    // call commands at the same time
+    const title = await browser.getTitle()
+    expect(title).toEqual(['JSON', 'JSON'])
+
+    // click on an element at the same time
+    const elem = await browser.$('#someElem')
+    await elem.click()
+
+    // only click with one browser (Firefox)
+    await elem.getInstance('myFirefoxBrowser').click()
+})
